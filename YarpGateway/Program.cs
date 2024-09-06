de9390e0499aeff84832498c6c0088c1c35aa28d
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2024-09-06 01:19:16 -0500

    Improve console log

--- YarpGateway/Program.cs
+++ YarpGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging.Console;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -5,9 +7,18 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddLogging(loggingBuilder => loggingBuilder
+            .ClearProviders()
+            .AddSimpleConsole(conf =>
+            {
+                conf.ColorBehavior = LoggerColorBehavior.Enabled;
+                conf.SingleLine = true;
+                conf.TimestampFormat = "HH:mm:ss ";
+            }));
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
